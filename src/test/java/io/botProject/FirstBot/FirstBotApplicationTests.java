9fb897e53a616f89b767363ad2a506902c9fcc3d
Зелимхан Атаев <zelik1204@gmail.com>
2023-05-02 12:35:40 +0300

    let's start our adventure

---
+++ src/test/java/io/botProject/FirstBot/FirstBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.botProject.FirstBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
